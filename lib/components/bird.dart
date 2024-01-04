16bbe71b16135d3f34f5d7bcd2aaaa9833f7ac86
Janik Kahle <Janik_K@gmx.de>
2024-01-04 14:36:05 +0100

    added Sprites - Bird - Pipes - Background - Ground added Overlays - mainMenu - gameOver added Logic for dynamic Pipes added HitBoxes added Start and Restart Logic

---
+++ lib/components/bird.dart
@@ -0,0 +1,66 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+import 'package:flappy_bird/game/assets.dart';
+import 'package:flappy_bird/game/bird_movement.dart';
+import 'package:flappy_bird/game/configuration.dart';
+import 'package:flappy_bird/game/flappy_bird_game.dart';
+import 'package:flutter/material.dart';
+
+class Bird extends SpriteGroupComponent<BirdMovement>
+    with HasGameRef<FlappyBirdGame>, CollisionCallbacks {
+  @override
+  Future<void> onLoad() async {
+    final birdMidFlap = await gameRef.loadSprite(Assets.birdMidFlap);
+    final birdUpFlap = await gameRef.loadSprite(Assets.birdUpFlap);
+    final birdDownFlap = await gameRef.loadSprite(Assets.birdDownFlap);
+
+    size = Vector2(50, 40);
+    position = Vector2(50, gameRef.size.y / 2 - size.y / 2);
+    current = BirdMovement.middle;
+    sprites = {
+      BirdMovement.middle: birdMidFlap,
+      BirdMovement.up: birdUpFlap,
+      BirdMovement.down: birdDownFlap,
+    };
+
+    add(CircleHitbox());
+  }
+
+  void fly() {
+    add(
+      MoveByEffect(
+        Vector2(0, Config.gravity),
+        EffectController(duration: 0.2, curve: Curves.decelerate),
+        onComplete: () => current = BirdMovement.down,
+      ),
+    );
+    current = BirdMovement.up;
+  }
+
+  @override
+  void onCollisionStart(
+    Set<Vector2> intersectionPoints,
+    PositionComponent other,
+  ) {
+    super.onCollisionStart(intersectionPoints, other);
+    gameOver();
+    debugPrint('Collision detected');
+  }
+
+  void reset() {
+    position = Vector2(50, gameRef.size.y / 2 - size.y / 2);
+  }
+
+  void gameOver() {
+    gameRef.overlays.add('gameOver');
+    gameRef.pauseEngine();
+    game.isHit = true;
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position.y += Config.birdVelocity * dt;
+  }
+}
---
+++ lib/game/flappy_bird_game.dart
@@ -0,0 +1,39 @@
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flappy_bird/components/background.dart';
+import 'package:flappy_bird/components/bird.dart';
+import 'package:flappy_bird/components/ground.dart';
+import 'package:flappy_bird/components/pipe_group.dart';
+import 'package:flappy_bird/game/configuration.dart';
+
+class FlappyBirdGame extends FlameGame with TapDetector, HasCollisionDetection {
+  FlappyBirdGame();
+
+  late Bird bird;
+  Timer interval = Timer(Config.pipeInterval, repeat: true);
+  bool isHit = false;
+
+  @override
+  Future<void> onLoad() async {
+    addAll([
+      Background(),
+      Ground(),
+      bird = Bird(),
+    ]);
+
+    interval.onTick = () => add(PipeGroup());
+  }
+
+  @override
+  void onTap() {
+    super.onTap();
+    bird.fly();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    interval.update(dt);
+  }
+}
